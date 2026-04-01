2e13c5b14559c15dcdcbf7e423e22c2f834f374e
Laibah <90483525+sewercat@users.noreply.github.com>
2026-04-02 00:54:03 +0500

    Remove Firebase initialization code
    Removed Firebase initialization and comments.

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDp8WqWrU93gtMWtlGW4CZYZViearEjvsk",
   authDomain: "dadeeofdathrifts.firebaseapp.com",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "540336185675",
   appId: "1:540336185675:web:d76993d3cb4baa61d008ce"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
