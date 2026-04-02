5a70655be7fbf02b3576ef467242fec3a781a6bf
Laibah <90483525+sewercat@users.noreply.github.com>
2026-04-02 09:20:54 +0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDp8WqWrU93gtMWtlGW4CZYZViearEjvsk",
-  authDomain: "dadeeofdathrifts.firebaseapp.com",
-  projectId: "dadeeofdathrifts",
-  storageBucket: "dadeeofdathrifts.firebasestorage.app",
-  messagingSenderId: "540336185675",
-  appId: "1:540336185675:web:d76993d3cb4baa61d008ce"
-};
